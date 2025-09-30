add0ba1bb0275ff0e57fe3be15aeeb6f2a5ced36
Kartavya728 <kartavya.xenon@gmail.com>
2025-09-30 14:37:44 +0530

    modify backend with react agent with some bugs

--- Backend/core/views.py
+++ Backend/core/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import StreamingHttpResponse
 from llm_functions.llm_service import generate_text_with_gemini
 
-
 def home(request):
     if request.method == "GET" and request.GET.get("user_input"):
         user_input = request.GET.get("user_input")
@@ -12,7 +11,7 @@ def home(request):
         )
         response["Cache-Control"] = "no-cache"
         response["X-Accel-Buffering"] = "no"
+        response["Connection"] = "keep-alive"
         return response
 
     return render(request, "core/home.html")
-
